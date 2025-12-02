470cfd53fd9b16269d8f82708576bf89a82a5453
ashotvardanyan <ash_077@icloud.com>
2025-12-03 01:28:41 +0400

    make main logic of each dot click, circle rotation and each dot animations

--- timeline-app/src/components/Timeline/TimelineCircle.tsx
+++ timeline-app/src/components/Timeline/TimelineCircle.tsx
@@ -1,9 +1,50 @@
+import type { FC } from 'react';
 import cls from './timeline.module.scss';
+import { timelinePoints } from '../../mocks/timelinePoints';
 
-const TimelineCircle = () => {
+interface ITimelineCircle {
+    rotation: number;
+    activePoint: number;
+    handleDotClick: (index: number) => void;
+}
+
+const TimelineCircle: FC<ITimelineCircle> = (
+    {
+        rotation,
+        activePoint,
+        handleDotClick
+    }
+) => {
+    const anglePerDot = 360 / timelinePoints.length;
+
     return (
         <div className={cls.circle__container}>
-            <div className={cls.circle}></div>
+            <div
+                className={cls.circle}
+                style={{transform: `rotate(${rotation}deg)`}}
+            >
+                {timelinePoints.map((point, i) => {
+                    const radius = 265;
+                    const angle = anglePerDot * i;
+
+                    return (
+                        <div
+                            className={cls.each__dot}
+                            style={{transform: `rotate(${angle}deg) translate(${radius}px) rotate(${-angle - rotation}deg)`}}
+                            onClick={() => handleDotClick(i)}
+                        >
+                            <div
+                                className={`${cls.dot__wrapper} ${activePoint === i ? cls.active : ''}`}
+                            >
+                                <div className={cls.dot__content}>
+                                    <span className={cls.dot_number}>{point.id}</span>
+                                    <div className={cls.dot_title}>{point.title}</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
