a566d4d3689aabf4d7685b6dd1294504167e02cc
ashotvardanyan <ash_077@icloud.com>
2025-12-03 01:22:56 +0400

    create circle

---
+++ timeline-app/src/components/Timeline/TimelineCircle.tsx
@@ -0,0 +1,11 @@
+import cls from './timeline.module.scss';
+
+const TimelineCircle = () => {
+    return (
+        <div className={cls.circle__container}>
+            <div className={cls.circle}></div>
+        </div>
+    )
+}
+
+export default TimelineCircle;
--- timeline-app/src/components/Timeline/TimelineComponent.tsx
+++ timeline-app/src/components/Timeline/TimelineComponent.tsx
@@ -1,10 +1,13 @@
 import cls from './timeline.module.scss';
+import TimelineCircle from "./TimelineCircle";
 
 const TimelineComponent = () => {
+
     return (
         <section className={cls.wrapper}>
             <div className={cls.timeline__container}>
                 <h1 className={cls.title__heading}>Исторические даты</h1>
+                <TimelineCircle/>
             </div>
         </section>
     )
